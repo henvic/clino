b3bf912f4b5fb4ef640ac3156ce8bd9f1e110c7d
Henrique Vicente <henriquevicente@gmail.com>
2021-06-20 14:42:28 +0200

    ci: moving to GitHub Actions and updating CI scripts.

---
+++ scripts/ci-lint-install.sh
@@ -0,0 +1,6 @@
+source scripts/lib.sh
+
+ensure_go_binary golang.org/x/lint/golint
+ensure_go_binary honnef.co/go/tools/cmd/staticcheck
+ensure_go_binary github.com/securego/gosec/cmd/gosec
+ensure_go_binary github.com/client9/misspell/cmd/misspell
